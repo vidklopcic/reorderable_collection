59163b069820a05b694c7e231015343f7b85f38b
vidklopcic <klopcic.vid@gmail.com>
2022-10-13 14:40:01 +0200

    null safety

--- lib/reordeable_collection.dart
+++ lib/reordeable_collection.dart
@@ -5,7 +5,7 @@ import 'package:gm5_utils/gm5_utils.dart';
 typedef ReordeableCollectionGestureDetectorBuilder = Widget Function(Widget child);
 typedef ReordeableCollectionItemBuilder<T> = Widget Function(
   BuildContext context,
-  Key key,
+  Key? key,
   ReordeableCollectionGestureDetectorBuilder dragDetector,
   int index,
 );
@@ -53,21 +53,21 @@ class ReordeableCollection<T> extends StatefulWidget {
   final ReorderableCollectionOnReorder onReorder;
 
   /// overrides default preview from the itemBuilder
-  final DragPreviewBuilder dragPreviewBuilder;
+  final DragPreviewBuilder? dragPreviewBuilder;
 
   /// widget marking the potential drop target
-  final DropPlaceholderBuilder dropPlaceholderBuilder;
+  final DropPlaceholderBuilder? dropPlaceholderBuilder;
 
   /// Builds widgets directly behind the visible collection items.
   /// Can be used to add effects (eg. outline using OverflowBox or background if item is transparent)
-  final ShadowItemBuilder shadowItemBuilder;
+  final ShadowItemBuilder? shadowItemBuilder;
 
   const ReordeableCollection({
-    Key key,
-    @required this.collectionBuilder,
-    @required this.itemBuilder,
-    @required this.onReorder,
-    @required this.itemCount,
+    super.key,
+    required this.collectionBuilder,
+    required this.itemBuilder,
+    required this.onReorder,
+    required this.itemCount,
     this.duration = const Duration(milliseconds: 200),
     this.curve = Curves.easeInOutExpo,
     this.reorderType = ReordeableCollectionReorderType.reorder,
@@ -76,45 +76,45 @@ class ReordeableCollection<T> extends StatefulWidget {
     this.dragPreviewBuilder,
     this.dropPlaceholderBuilder,
     this.shadowItemBuilder,
-  }) : super(key: key);
+  });
 
   @override
   ReordeableCollectionState createState() => ReordeableCollectionState<T>();
 }
 
 class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with SingleTickerProviderStateMixin {
-  ReordeableCollectionItemBuilderWrapper<T> builderWrapper;
+  ReordeableCollectionItemBuilderWrapper<T>? builderWrapper;
 
   Map<int, Rect> _initialBounds = {};
   Map<int, Offset> _targetOffsets = {};
   Map<int, Offset> _currentOffsets = {};
   _ElementKeys currentKeys = _ElementKeys();
-  Offset _cursorOffset;
-  int _from;
-  int _to;
+  Offset? _cursorOffset;
+  int? _from;
+  int? _to;
   Offset _dragOffset = Offset.zero;
   GlobalKey _collectionKey = GlobalKey();
   GlobalKey _shadowCollectionKey = GlobalKey();
 
-  OverlayEntry _overlay;
+  OverlayEntry? _overlay;
 
   ScrollController _scrollController = ScrollController();
   ScrollController _shadowScrollController = ScrollController();
   bool _disableScrolling = false;
-  double _startScrollOffset = 0;
-  Rect _collectionBounds;
+  double? _startScrollOffset = 0;
+  Rect? _collectionBounds;
 
-  AnimationController _rearrangeAnimationController;
-  Animation<double> _rearrangeAnimation;
+  AnimationController? _rearrangeAnimationController;
+  Animation<double>? _rearrangeAnimation;
 
-  int _shadowIndexOffset;
-  List<Widget> _shadowChildren;
+  int? _shadowIndexOffset;
+  List<Widget>? _shadowChildren;
 
-  List<Rect> _shadowBounds = [];
+  List<Rect?> _shadowBounds = [];
 
   bool get isDragging => _from != null;
 
-  Widget _cachedCollection;
+  Widget? _cachedCollection;
 
   @override
   Widget build(BuildContext context) {
@@ -125,21 +125,21 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     return Stack(
       children: [
         Positioned.fill(child: _shadowWidget()),
-        _cachedCollection,
+        if (_cachedCollection != null) _cachedCollection!,
       ],
     );
   }
 
   Offset _calcTransitionOffset(int index) {
-    Offset oTo = _targetOffsets[index];
-    Offset oFrom = _initialBounds[index]?.center;
+    Offset? oTo = _targetOffsets[index];
+    Offset? oFrom = _initialBounds[index]?.center;
     Offset oCurr = _currentOffsets[index] ?? Offset.zero;
     if (oTo == null || oFrom == null) {
       oTo = Offset.zero;
       oFrom = Offset.zero;
     }
     Offset oTarg = oTo - oFrom;
-    return Tween(begin: oCurr, end: oTarg).animate(_rearrangeAnimation).value;
+    return Tween(begin: oCurr, end: oTarg).animate(_rearrangeAnimation!).value;
   }
 
   @override
@@ -152,9 +152,9 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
   void initState() {
     super.initState();
     _rearrangeAnimationController = AnimationController(vsync: this, duration: widget.duration);
-    _rearrangeAnimation = CurvedAnimation(parent: _rearrangeAnimationController, curve: widget.curve);
+    _rearrangeAnimation = CurvedAnimation(parent: _rearrangeAnimationController!, curve: widget.curve);
     builderWrapper = (context, index) => AnimatedBuilder(
-          animation: _rearrangeAnimation,
+          animation: _rearrangeAnimation!,
           builder: (ctx, child) {
             if (_from == index)
               return Transform.translate(
@@ -181,7 +181,7 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     // couple shadow scroll with real layout
     _scrollController.addListener(() {
       if (_startScrollOffset == null) return;
-      _shadowScrollController.jumpTo(_scrollController.offset - _startScrollOffset);
+      _shadowScrollController.jumpTo(_scrollController.offset - _startScrollOffset!);
     });
 
     buildCollection();
@@ -189,15 +189,15 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
 
   int toFutureIndex(int index) {
     if (_from == null || _to == null) return index;
-    if (index == _from) return _to;
+    if (index == _from) return _to!;
     if (widget.reorderType == ReordeableCollectionReorderType.swap) {
       if (index == _from)
-        index = _to;
-      else if (index == _to) index = _from;
+        index = _to!;
+      else if (index == _to) index = _from!;
     } else {
-      if (index > _from && index <= _to) {
+      if (index > _from! && index <= _to!) {
         index -= 1;
-      } else if (index < _from && index >= _to) {
+      } else if (index < _from! && index >= _to!) {
         index += 1;
       }
     }
@@ -208,14 +208,14 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     if (_from == null || _to == null) return index;
     if (widget.reorderType == ReordeableCollectionReorderType.swap) {
       if (index == _from)
-        index = _to;
-      else if (index == _to) index = _from;
+        index = _to!;
+      else if (index == _to) index = _from!;
     } else {
       if (index == _to) {
-        index = _from;
-      } else if (index >= _from && index < _to) {
+        index = _from!;
+      } else if (index >= _from! && index < _to!) {
         index += 1;
-      } else if (index <= _from && index > _to) {
+      } else if (index <= _from! && index > _to!) {
         index -= 1;
       }
     }
@@ -226,12 +226,12 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     if (_from == null || _to == null) return index;
     if (widget.reorderType == ReordeableCollectionReorderType.swap) {
       if (index == _from)
-        index = _to;
-      else if (index == _to) index = _from;
+        index = _to!;
+      else if (index == _to) index = _from!;
     } else {
-      if (index > _from && index <= _to) {
+      if (index > _from! && index <= _to!) {
         index -= 1;
-      } else if (index < _from && index >= _to) {
+      } else if (index < _from! && index >= _to!) {
         index += 1;
       }
     }
@@ -251,7 +251,7 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
             _overlay = OverlayEntry(
               builder: (ctx) => Material(type: MaterialType.transparency, child: _draggableChild(index)),
             );
-            Overlay.of(context).insert(_overlay);
+            Overlay.of(context)?.insert(_overlay!);
 
             _startShadowWidget();
 
@@ -277,7 +277,7 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
         );
   }
 
-  Rect _renderBoxToBounds(RenderBox box) {
+  Rect? _renderBoxToBounds(RenderBox? box) {
     if (box == null) return null;
     Offset topLeft = box.localToGlobal(Offset.zero);
     Offset bottomRight = topLeft + box.size.bottomRight(Offset.zero);
@@ -286,9 +286,9 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
 
   void _updateHitObject(GlobalKey draggingKey) {
     if (_from == null) return;
-    Offset renderCenter = _dragOffset + _initialBounds[_from].center;
+    Offset renderCenter = _dragOffset + _initialBounds[_from]!.center;
     for (int index in _initialBounds.keys) {
-      if (_initialBounds[index].contains(renderCenter)) {
+      if (_initialBounds[index]!.contains(renderCenter)) {
         _setToIndex(index);
         return;
       }
@@ -313,33 +313,33 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
 
     for (int index in _initialBounds.keys) {
       _updateShadowBounds();
-      _targetOffsets[index] = _shadowBounds[index - _shadowIndexOffset].center;
+      _targetOffsets[index] = _shadowBounds[index - _shadowIndexOffset!]!.center;
     }
 
-    _rearrangeAnimationController.forward(from: 0);
+    _rearrangeAnimationController!.forward(from: 0);
   }
 
   void _updateBounds() {
-    RenderObject renderObject = _collectionKey.currentContext?.findRenderObject();
+    RenderObject? renderObject = _collectionKey.currentContext?.findRenderObject();
     if (renderObject == null || !renderObject.attached) {
       throw Exception('Collection key is not attached. Did you forget to use the ReordeableCollectionBuilder key?');
     }
-    _collectionBounds = _renderBoxToBounds(renderObject);
+    _collectionBounds = _renderBoxToBounds(renderObject as RenderBox);
 
     for (GlobalKey key in currentKeys.keys) {
-      RenderObject renderObject = key.currentContext?.findRenderObject();
+      RenderObject? renderObject = key.currentContext?.findRenderObject();
       if (renderObject == null || !renderObject.attached) continue;
-      _initialBounds[currentKeys.indexForKey(key)] = _renderBoxToBounds(renderObject);
+      _initialBounds[currentKeys.indexForKey(key)] = _renderBoxToBounds(renderObject as RenderBox)!;
     }
   }
 
   void _updateShadowBounds() {
-    _shadowBounds = List.filled(_shadowChildren.length, null);
-    for (int i = 0; i < _shadowChildren.length; i++) {
-      final key = _shadowChildren[i].key as GlobalKey;
-      RenderObject renderObject = key.currentContext?.findRenderObject();
+    _shadowBounds = List.filled(_shadowChildren!.length, null);
+    for (int i = 0; i < _shadowChildren!.length; i++) {
+      final key = _shadowChildren![i].key as GlobalKey;
+      RenderObject? renderObject = key.currentContext?.findRenderObject();
       if (renderObject == null || !renderObject.attached) continue;
-      _shadowBounds[i] = _renderBoxToBounds(renderObject);
+      _shadowBounds[i] = _renderBoxToBounds(renderObject as RenderBox);
     }
   }
 
@@ -350,22 +350,22 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     if (_to == null) {
       _to = _from;
     }
-    _currentOffsets[_from] = _dragOffset;
-    int shadowIndex = _from - _shadowIndexOffset;
-    if (shadowIndex <= _shadowBounds?.length ?? 0)
-      _targetOffsets[_from] = _shadowBounds[shadowIndex].center;
+    _currentOffsets[_from!] = _dragOffset;
+    int shadowIndex = _from! - _shadowIndexOffset!;
+    if (shadowIndex <= _shadowBounds.length)
+      _targetOffsets[_from!] = _shadowBounds[shadowIndex]!.center;
     else
-      _targetOffsets[_from] = _initialBounds[_from].center;
-    int from = _from;
+      _targetOffsets[_from!] = _initialBounds[_from!]!.center;
+    int from = _from!;
     _from = null;
-    _overlay.remove();
-    await _rearrangeAnimationController.forward(from: 0);
+    _overlay!.remove();
+    await _rearrangeAnimationController!.forward(from: 0);
     if (from != _to) {
-      if (widget.onReorder != null) widget.onReorder(from, _to);
+      widget.onReorder(from, _to!);
       if (widget.reorderType == ReordeableCollectionReorderType.swap) {
-        currentKeys.swap(from, _to);
+        currentKeys.swap(from, _to!);
       } else {
-        currentKeys.move(from, _to);
+        currentKeys.move(from, _to!);
       }
     }
     setState(() {
@@ -383,16 +383,16 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     return Align(
       alignment: Alignment.topLeft,
       child: SizedBox(
-        width: _initialBounds[index].width,
-        height: _initialBounds[index].height,
+        width: _initialBounds[index]!.width,
+        height: _initialBounds[index]!.height,
         child: AnimatedBuilder(
-          animation: _rearrangeAnimationController,
+          animation: _rearrangeAnimationController!,
           builder: (ctx, child) => Transform.translate(
             offset: _from == null
-                ? (_initialBounds[index].topLeft + _calcTransitionOffset(index))
-                : (_dragOffset + _initialBounds[index].topLeft),
+                ? (_initialBounds[index]!.topLeft + _calcTransitionOffset(index))
+                : (_dragOffset + _initialBounds[index]!.topLeft),
             child: widget.dragPreviewBuilder != null
-                ? widget.dragPreviewBuilder(context, _from, _cursorOffset)
+                ? widget.dragPreviewBuilder!(context, _from!, _cursorOffset!)
                 : widget.itemBuilder(
                     context,
                     null,
@@ -411,13 +411,13 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
       context,
       _shadowCollectionKey,
       (c, i) {
-        int fi = fromFutureIndex(i + _shadowIndexOffset) - _shadowIndexOffset;
+        int fi = fromFutureIndex(i + _shadowIndexOffset!) - _shadowIndexOffset!;
         // print('$i -> $fi');
-        return _shadowChildren[fi];
+        return _shadowChildren![fi];
       },
       _shadowScrollController,
       true,
-      _shadowChildren.length,
+      _shadowChildren!.length,
     );
   }
 
@@ -426,9 +426,9 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
       _startScrollOffset = _scrollController.offset;
       double scrollOffset;
       if (widget.scrollDirection == Axis.vertical) {
-        scrollOffset = _collectionBounds.top - _initialBounds.values.map((e) => e.top).reduce(math.min);
+        scrollOffset = _collectionBounds!.top - _initialBounds.values.map((e) => e.top).reduce(math.min);
       } else {
-        scrollOffset = _collectionBounds.left - _initialBounds.values.map((e) => e.left).reduce(math.min);
+        scrollOffset = _collectionBounds!.left - _initialBounds.values.map((e) => e.left).reduce(math.min);
       }
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         _shadowScrollController.jumpTo(scrollOffset);
@@ -439,16 +439,16 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
 
     _shadowChildren = [];
     for (int index in _initialBounds.keys.toList()..sort()) {
-      _shadowChildren.add(
+      _shadowChildren!.add(
         SizedBox(
           key: GlobalKey(),
-          width: _initialBounds[index].width,
-          height: _initialBounds[index].height,
+          width: _initialBounds[index]!.width,
+          height: _initialBounds[index]!.height,
           child: index == _from
               ? widget.dropPlaceholderBuilder != null
-                  ? widget.dropPlaceholderBuilder(context, _from, _to)
+                  ? widget.dropPlaceholderBuilder!(context, _from!, _to!)
                   : widget.shadowItemBuilder != null
-                      ? widget.shadowItemBuilder(context, index)
+                      ? widget.shadowItemBuilder!(context, index)
                       : null
               : null,
         ),
@@ -463,7 +463,7 @@ class ReordeableCollectionState<T> extends State<ReordeableCollection<T>> with S
     _cachedCollection = widget.collectionBuilder(
       context,
       _collectionKey,
-      builderWrapper,
+      builderWrapper!,
       _scrollController,
       _disableScrolling,
       widget.itemCount,
@@ -486,14 +486,14 @@ class _ElementKeys {
     );
   }
 
-  int indexForKey(GlobalKey key) => _keys[key];
+  int indexForKey(GlobalKey key) => _keys[key]!;
 
   List<GlobalKey> get keys => _keys.keys.toList();
 
   void swap(int from, int to) {
     if (from == to) return;
-    final _fk = _indexes[from];
-    final _tk = _indexes[to];
+    final _fk = _indexes[from]!;
+    final _tk = _indexes[to]!;
     _indexes[from] = _tk;
     _indexes[to] = _fk;
     _keys[_tk] = from;
@@ -502,14 +502,14 @@ class _ElementKeys {
 
   void move(int from, int to) {
     if (from == to) return;
-    GlobalKey replacedKey = _indexes[to];
-    _indexes[to] = _indexes[from];
-    _keys[_indexes[to]] = to;
+    GlobalKey replacedKey = _indexes[to]!;
+    _indexes[to] = _indexes[from]!;
+    _keys[_indexes[to]!] = to;
 
     int diff = to - from;
     for (int i = to - diff.sign; (i - to).abs() <= diff.abs(); i -= diff.sign) {
       GlobalKey currentKey = replacedKey;
-      replacedKey = _indexes[i];
+      replacedKey = _indexes[i]!;
       _indexes[i] = currentKey;
       _keys[currentKey] = i;
     }
